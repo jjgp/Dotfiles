06cf507ecf23f0a7254ed945732b413bf78e4a0e
Jason Prasad <jwalapr@gmail.com>
2017-03-08 14:25:04 -0500

    Removing auto push script in favor of msgs

--- git-auto-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-repos=${1:?"Git repos not provided."} 
-if [[ `git -C $repos status --porcelain` ]]; then
-  git -C $repos add .
-  git -C $repos commit -m "$(whoami) on $(basename $(hostname) .local) at $(date)"
-  git -C $repos push origin master
-fi
-
